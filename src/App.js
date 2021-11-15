910f21344018e48a6c80328641930e9151bce87d
Prashant kumar <prahsantk7@chetu.com>
2021-11-15 15:20:29 +0530

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import "./App.css";
-
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="box">
-      <h1>Todo App</h1>
-      <div className="todo-app">
-        <input type="text" placeholder="input your Task"></input>
-        <button>Add Task</button>
-        <ul>
-          <li></li>
-        </ul>
-      </div>
+    <div className="app">
+      <Header />
     </div>
   );
 }
