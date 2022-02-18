38c29cd95610ebf385c41ff3fc8cc0e48f84f6a3
prashant kumar <prashantk7@chetu.com>
2022-02-18 15:58:11 +0530

    Updates

---
+++ precache-manifest.ed017f9cd02df72c4a70e21f3bf57015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad0aab3c69cd6346db18fbfa380d7b6",
+    "url": "/Task_9_React_todo_app/index.html"
+  },
+  {
+    "revision": "307625f94acf340c489b",
+    "url": "/Task_9_React_todo_app/static/css/main.b6aa6df0.chunk.css"
+  },
+  {
+    "revision": "872bd3feace22ec13d5c",
+    "url": "/Task_9_React_todo_app/static/js/2.450ee5bb.chunk.js"
+  },
+  {
+    "revision": "f76f15b609472adf531e",
+    "url": "/Task_9_React_todo_app/static/js/3.bfc2c5aa.chunk.js"
+  },
+  {
+    "revision": "307625f94acf340c489b",
+    "url": "/Task_9_React_todo_app/static/js/main.4bd0f1f7.chunk.js"
+  },
+  {
+    "revision": "fd761793e5f35fc470d8",
+    "url": "/Task_9_React_todo_app/static/js/runtime~main.792ec713.js"
+  }
+]);
